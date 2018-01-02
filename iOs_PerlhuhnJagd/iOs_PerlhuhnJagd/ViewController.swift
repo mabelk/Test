c19a0219c0a0e90f3e1f1602fab2c4822c662971
MacDevGames <macdevgames@MacDevGamess-iMac.local>
2018-01-02 12:16:59 +0100

    new Project iOs_PerlhuhnJagd

---
+++ iOs_PerlhuhnJagd/iOs_PerlhuhnJagd/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  iOs_PerlhuhnJagd
+//
+//  Created by MacDevGames on 31.12.17.
+//  Copyright © 2017 MacDevGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func btnHier_Click(_ sender: Any) {
+        //Start new View for Hunting Perlhuhns
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
